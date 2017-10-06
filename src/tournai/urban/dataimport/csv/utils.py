7c4f4ace469b25f1143d874b3e97f727227759da
jjaumotte <julien.jaumotte@imio.be>
2017-10-06 09:30:57 +0200

    Update utils with new find parcels method

--- src/tournai/urban/dataimport/csv/utils.py
+++ src/tournai/urban/dataimport/csv/utils.py
@@ -300,9 +300,9 @@ def create_parcel_in_notary_letter(parcel, container):
     for k, v in parcel_args.iteritems():
         searchview.context.REQUEST[k] = v
     # check if we can find a parcel in the db cadastre with these infos
-    found = searchview.findParcel(**parcel_args)
+    found = searchview.search_parcels_custom(**parcel_args)
     if not found:
-        found = searchview.findParcel(browseoldparcels=True, **parcel_args)
+        found = searchview.search_parcels_custom(True, **parcel_args)
 
     if len(found) == 1 and parcel.has_same_attribute_values(found[0].__dict__):
         parcel_args['divisionCode'] = parcel_args['division']
@@ -412,9 +412,9 @@ def create_parcel(parcelling, section1, num1, num1suite, division):
         for k, v in parcel_args.iteritems():
             searchview.context.REQUEST[k] = v
         #check if we can find a parcel in the db cadastre with these infos
-        found = searchview.findParcel(**parcel_args)
+        found = searchview.search_parcels_custom(**parcel_args)
         if not found:
-            found = searchview.findParcel(browseoldparcels=True, **parcel_args)
+            found = searchview.search_parcels_custom(True, **parcel_args)
 
         if len(found) == 1 and parcel.has_same_attribute_values(found[0].__dict__):
             parcel_args['divisionCode'] = parcel_args['division']
