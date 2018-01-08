dc01dd4a185c428626c4f24f0b050b997999ffdf
jjaumotte <julien.jaumotte@imio.be>
2018-01-08 16:09:06 +0100

    Revert, already done with generic imio import.

--- src/tournai/urban/dataimport/notaryletters/settings.py
+++ src/tournai/urban/dataimport/notaryletters/settings.py
@@ -3,9 +3,7 @@
 from tournai.urban.dataimport.architects.importer import ArchitectsImporter
 from imio.urban.dataimport.csv.settings import CSVImporterFromImportSettings
 from tournai.urban.dataimport.csv.utils import create_notary_letters
-import collective.noindexing
-import ConfigParser as configparser
-import os
+
 
 class ArchitectsImporterFromImportSettings(CSVImporterFromImportSettings):
     """ """
@@ -14,10 +12,6 @@ class ArchitectsImporterFromImportSettings(CSVImporterFromImportSettings):
         """
         """
         super(CSVImporterFromImportSettings, self).__init__(settings_form, importer_class)
-        # parse option
-        config = configparser.ConfigParser()
-        config.read(os.path.join(os.path.abspath(os.path.dirname(__file__)), 'utils.cfg'))
-        self.no_index = config.get("no_index", "active") if config.get("no_index", "active") else 0
 
     def get_importer_settings(self):
         """
@@ -31,13 +25,6 @@ class ArchitectsImporterFromImportSettings(CSVImporterFromImportSettings):
         }
 
         settings.update(csv_settings)
-
-        if self.no_index:
-            collective.noindexing.patches.apply()
-
         create_notary_letters()
-
-        if self.no_index:
-            collective.noindexing.patches.unapply()
 
         return settings
